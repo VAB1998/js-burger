486e1672ffb8387c02b03e6d14900278c79fb43b
VAB1998 <blanco.vincenzo@outlook.com>
2021-08-30 21:37:15 +0200

    Added if statement to check the text area

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,20 @@
 //Variables Initilization
+
 let nameYourBurger = document.getElementById("name_your_burger");
 let calculate = document.getElementById("calculate_button");
-
-console.log(nameYourBurger);
+let addIngredients = document.getElementsByClassName("add_ingredient");
+let discountCoupon = document.getElementById("discount_coupon");
+const validCoupons = ["Aba", "Bcb", "Cdc"];
 console.log(calculate);
+
+calculate.addEventListener("click", function(){
+    console.log(nameYourBurger.value.length)
+    //If the text area is empty:
+    if(nameYourBurger.value.length == 0){
+        alert("Inserisci il nome del burger")
+    } else{
+        console.log("Giusto");
+    }
+});
+
+

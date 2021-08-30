88b3cf0d7d7e384cdcb196680535ad2a58bb5383
VAB1998 <blanco.vincenzo@outlook.com>
2021-08-30 18:50:26 +0200

    Declarated variables on JS

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+//Variables Initilization
+let nameYourBurger = document.getElementById("name_your_burger");
+let calculate = document.getElementById("calculate_button");
+
+console.log(nameYourBurger);
+console.log(calculate);

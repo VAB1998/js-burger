3b48d46e6daabdaea8f13cf74b66b268d695cc52
VAB1998 <blanco.vincenzo@outlook.com>
2021-08-31 15:06:21 +0200

    Added new fetures

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,24 @@
-//Variables Initilization
-//!!!!!!!!!! SE una variabile ci serve solo su una funzione BISOGNA inserirla SOLO all'interno della funzione
-let nameYourBurger = document.getElementById("name_your_burger");
 let calculate = document.getElementById("calculate_button");
-let addIngredients = document.getElementsByClassName("add_ingredient");
-let discountCoupon = document.getElementById("discount_coupon");
-const validCoupons = ["Aba", "Bcb", "Cdc"];
-console.log(calculate);
-console.log(nameYourBurger);
-let price = 0;
+
 calculate.addEventListener("click", function(){
-     
-    //If the text area is empty:
+    let nameYourBurger = document.getElementById("name_your_burger");
+    let addIngredients = document.getElementsByClassName("add_ingredient");
+    let discountCoupon = document.getElementById("discount_coupon");
+    const validCoupons = ["Aba", "Bcb", "Cdc"];
+    let price = 0;
+
+
+    //If the text area is empty: let the user know
     if(nameYourBurger.value.length === 0){
-        alert("Inserisci il nome del burger");
+        document.getElementById("name_your_burger").classList.add("is-invalid");
+        document.getElementById("required_field_name_burger").classList.remove("d-none")
+        
         price = 0;
     } else{
+        document.getElementById("name_your_burger").classList.remove("is-invalid");
+        document.getElementById("required_field_name_burger").classList.add("d-none")
+        console.log(required_field_name_burger);
+        
         price = 50;
 
         //Iterating through each element with a specific class using the for loop index

bf89adb13ea594c71e8caea071fb2b3d80be04bd
VAB1998 <blanco.vincenzo@outlook.com>
2021-08-31 13:46:51 +0200

    JS Rev 1: code improved

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 //Variables Initilization
-
+//!!!!!!!!!! SE una variabile ci serve solo su una funzione BISOGNA inserirla SOLO all'interno della funzione
 let nameYourBurger = document.getElementById("name_your_burger");
 let calculate = document.getElementById("calculate_button");
 let addIngredients = document.getElementsByClassName("add_ingredient");
@@ -7,37 +7,29 @@ let discountCoupon = document.getElementById("discount_coupon");
 const validCoupons = ["Aba", "Bcb", "Cdc"];
 console.log(calculate);
 console.log(nameYourBurger);
-
+let price = 0;
 calculate.addEventListener("click", function(){
-    let price = 50;
+     
     //If the text area is empty:
-    if(nameYourBurger.value.length == 0){
-        alert("Inserisci il nome del burger")
+    if(nameYourBurger.value.length === 0){
+        alert("Inserisci il nome del burger");
         price = 0;
     } else{
-       
+        price = 50;
+
         //Iterating through each element with a specific class using the for loop index
         for(let i = 0; i < addIngredients.length; i++){
 
             if(addIngredients[i].checked == true) {
-                //console.log("Vero");
-                price += 4;
-            } else if(addIngredients[i].checked == false){
-                //console.log("Falso");
+                price += parseInt(addIngredients[i].value);
             } 
-            
         }
+
         //Comparing the list of coupons
-        for(let j = 0; j < validCoupons.length; j++){
-            if(discountCoupon.value == validCoupons[j]){
-                price = price * 0.8;
-            }
+        if(validCoupons.includes(discountCoupon.value)){
+            price = price * 0.8;
         }
-    
     }
     console.log("Prezzo: " + price.toFixed(2));
-    document.getElementById("your_price").innerHTML = price.toFixed(2);
-
+    document.getElementById("your_price").innerHTML = price.toFixed(2) + "&euro;";
 });
-
-

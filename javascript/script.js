bd5a5ed8c408215e0b2517314902ee01ca343809
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-01 18:06:57 +0200

    Rev. 1: Improved html and added new features in JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 let calculate = document.getElementById("calculate_button");
 
+
 calculate.addEventListener("click", function(){
     let nameYourBurger = document.getElementById("name_your_burger");
     let addIngredients = document.getElementsByClassName("add_ingredient");
@@ -14,9 +15,11 @@ calculate.addEventListener("click", function(){
         document.getElementById("required_field_name_burger").classList.remove("d-none")
         
         price = 0;
+        
     } else{
         document.getElementById("name_your_burger").classList.remove("is-invalid");
-        document.getElementById("required_field_name_burger").classList.add("d-none")
+        document.getElementById("name_your_burger").classList.add("is-valid");
+        document.getElementById("required_field_name_burger").classList.add("d-none");
         console.log(required_field_name_burger);
         
         price = 50;
@@ -31,8 +34,17 @@ calculate.addEventListener("click", function(){
 
         //Comparing the list of coupons
         if(validCoupons.includes(discountCoupon.value)){
+            document.getElementById("discount_coupon").classList.add("is-valid");
+            document.getElementById("discount_coupon").classList.remove("is-invalid");
             price = price * 0.8;
-        }
+        } else if(discountCoupon.value === ""){
+            document.getElementById("discount_coupon").classList.remove("is-valid");
+            document.getElementById("discount_coupon").classList.remove("is-invalid");
+        } else{
+            document.getElementById("discount_coupon").classList.add("is-invalid");
+            document.getElementById("discount_coupon").classList.remove("is-valid");
+        } 
+        console.log("valore stringa: " + discountCoupon.value);
     }
     console.log("Prezzo: " + price.toFixed(2));
     document.getElementById("your_price").innerHTML = price.toFixed(2) + "&euro;";
